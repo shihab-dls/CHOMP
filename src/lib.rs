d73c7204a7a8a105723e074a6d876e4e9ad12c9f
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-05-12 12:43:04 +0000

    Add connection utils

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
 pub(crate) mod tables;
+
+use sea_orm::{Database, DatabaseConnection, DbErr};
+use std::path::Path;
+
+#[derive(Debug, thiserror::Error)]
+pub enum ConnectionError {
+    #[error("Supplied path is not valid unicode.")]
+    InvalidPath,
+    #[error("Database produced error during connection attempt.")]
+    DatabaseError(#[from] DbErr),
+}
+
+pub async fn connect(path: impl AsRef<Path>) -> Result<DatabaseConnection, ConnectionError> {
+    let database_url = format!(
+        "sqlite://{}",
+        path.as_ref().to_str().ok_or(ConnectionError::InvalidPath)?
+    );
+    Ok(Database::connect(database_url).await?)
+}
