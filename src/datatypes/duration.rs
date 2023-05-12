1fde8413399c66e5608c759be9fcb8777f49f8a9
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-05-12 12:50:34 +0000

    Add DurationAsTimeText wrapper database type

--- src/datatypes/duration.rs
+++ src/datatypes/duration.rs
@@ -1,3 +1,4 @@
+use super::combination::AsSelfOr;
 use chrono::{Duration, NaiveTime};
 use derive_more::{Deref, DerefMut, From};
 use sea_orm::{
@@ -5,6 +6,8 @@ use sea_orm::{
     TryGetError, TryGetable, Value,
 };
 use std::{any::type_name, str::FromStr};
+
+pub type DurationAsVarious = AsSelfOr<DurationAsText, DurationAsTimeText>;
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Deref, DerefMut, From)]
 pub struct DurationAsText(Duration);
@@ -79,9 +82,94 @@ impl Nullable for DurationAsText {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Deref, DerefMut, From)]
+pub struct DurationAsTimeText(Duration);
+
+static DURATION_FORMAT_TIME: &str = "%H:%M:%S AM";
+
+impl FromStr for DurationAsTimeText {
+    type Err = chrono::ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        NaiveTime::parse_from_str(s, DURATION_FORMAT_TIME).map(|time| Self(time - NaiveTime::MIN))
+    }
+}
+
+impl ToString for DurationAsTimeText {
+    fn to_string(&self) -> String {
+        (NaiveTime::MIN + self.0)
+            .format(DURATION_FORMAT_TIME)
+            .to_string()
+    }
+}
+
+impl From<DurationAsTimeText> for Value {
+    fn from(value: DurationAsTimeText) -> Self {
+        Self::String(Some(Box::new(value.to_string())))
+    }
+}
+
+impl TryGetable for DurationAsTimeText {
+    fn try_get_by<I: sea_orm::ColIdx>(
+        res: &sea_orm::QueryResult,
+        index: I,
+    ) -> Result<Self, TryGetError> {
+        let text = String::try_get_by(res, index)?.trim().to_string();
+        if text.is_empty() {
+            Err(sea_orm::TryGetError::Null(type_name::<I>().to_string()))
+        } else {
+            text.parse().map_err(|err| {
+                sea_orm::TryGetError::DbErr(sea_orm::DbErr::Type(format!(
+                    "Could not parse '{}' as Duration using format '{}' for {:?}: {}",
+                    text, DURATION_FORMAT_TIME, index, err
+                )))
+            })
+        }
+    }
+}
+
+impl ValueType for DurationAsTimeText {
+    fn try_from(v: Value) -> Result<Self, sea_orm::sea_query::ValueTypeErr> {
+        match v {
+            Value::String(Some(val)) => val.parse().map_err(|_| sea_orm::sea_query::ValueTypeErr),
+            _ => Err(sea_orm::sea_query::ValueTypeErr),
+        }
+    }
+
+    fn type_name() -> String {
+        type_name::<Self>().to_string()
+    }
+
+    fn array_type() -> sea_orm::sea_query::ArrayType {
+        sea_orm::sea_query::ArrayType::String
+    }
+
+    fn column_type() -> sea_orm::ColumnType {
+        sea_orm::ColumnType::String(None)
+    }
+}
+
+impl Nullable for DurationAsTimeText {
+    fn null() -> Value {
+        String::null()
+    }
+}
+
+impl From<DurationAsTimeText> for DurationAsText {
+    fn from(value: DurationAsTimeText) -> Self {
+        Self(value.0)
+    }
+}
+
+impl From<DurationAsText> for DurationAsTimeText {
+    fn from(value: DurationAsText) -> Self {
+        Self(value.0)
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::DurationAsText;
+    use super::{DurationAsText, DurationAsTimeText};
     use chrono::Duration;
     use std::str::FromStr;
 
@@ -99,6 +187,22 @@ mod tests {
         assert_eq!(
             test_duration(),
             DurationAsText::from_str("03:14:15").unwrap().0
+        )
+    }
+
+    #[test]
+    fn duration_as_time_text_serializes() {
+        assert_eq!(
+            "03:14:15 AM",
+            DurationAsTimeText(test_duration()).to_string()
+        )
+    }
+
+    #[test]
+    fn duration_as_time_text_deserializes() {
+        assert_eq!(
+            test_duration(),
+            DurationAsTimeText::from_str("03:14:15 AM").unwrap().0
         )
     }
 }
