03d59c7d95dbcbbfc3b42b83167814d770d487f7
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-07-05 10:47:50 +0000

    Add (de)serializers to protocol structs

--- chimp_protocol/src/lib.rs
+++ chimp_protocol/src/lib.rs
@@ -7,12 +7,42 @@ pub struct Job {
     pub predictions_channel: String,
 }
 
+impl Job {
+    pub fn from_slice(v: &[u8]) -> Result<Self, serde_json::Error> {
+        serde_json::from_slice(v)
+    }
+
+    pub fn to_vec(&self) -> Result<Vec<u8>, serde_json::Error> {
+        serde_json::to_vec(&self)
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Predictions(pub Vec<Prediction>);
+
+impl Predictions {
+    pub fn from_slice(v: &[u8]) -> Result<Self, serde_json::Error> {
+        serde_json::from_slice(v)
+    }
+
+    pub fn to_vec(&self) -> Result<Vec<u8>, serde_json::Error> {
+        serde_json::to_vec(&self)
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Prediction {
     pub bbox: [f32; 4],
     pub label: i64,
     pub score: f32,
+}
+
+impl Prediction {
+    pub fn from_slice(v: &[u8]) -> Result<Self, serde_json::Error> {
+        serde_json::from_slice(v)
+    }
+
+    pub fn to_vec(&self) -> Result<Vec<u8>, serde_json::Error> {
+        serde_json::to_vec(&self)
+    }
 }
