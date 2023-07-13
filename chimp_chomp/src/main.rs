1c34818fed9098656d2a09952963c51b5bece24c
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-07-13 12:19:17 +0000

    Add thread option to control worker thread count

--- chimp_chomp/src/main.rs
+++ chimp_chomp/src/main.rs
@@ -35,89 +35,98 @@ struct Cli {
     /// The duration (in milliseconds) to wait after completing all jobs before shutting down.
     #[arg(long)]
     timeout: Option<u64>,
+    /// The number of worker threads to use
+    #[arg(long)]
+    threads: Option<usize>,
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     dotenvy::dotenv().ok();
     let args = Cli::parse();
     opencv::core::set_num_threads(0).unwrap();
 
-    let session = setup_inference_session(args.model).unwrap();
-    let input_width = session.inputs[0].dimensions[3].unwrap();
-    let input_height = session.inputs[0].dimensions[2].unwrap();
-    let batch_size = session.inputs[0].dimensions[0].unwrap().try_into().unwrap();
-
-    let rabbitmq_client = setup_rabbitmq_client(args.rabbitmq_url).await.unwrap();
-    let job_channel = rabbitmq_client.create_channel().await.unwrap();
-    let response_channel = rabbitmq_client.create_channel().await.unwrap();
-    let mut job_consumer = setup_job_consumer(job_channel, args.rabbitmq_channel)
-        .await
-        .unwrap();
-
-    let (chimp_image_tx, chimp_image_rx) = tokio::sync::mpsc::channel(batch_size);
-    let (well_image_tx, mut well_image_rx) = tokio::sync::mpsc::channel(batch_size);
-    let (well_location_tx, mut well_location_rx) =
-        tokio::sync::mpsc::unbounded_channel::<(Circle, Job)>();
-    let (prediction_tx, mut prediction_rx) = tokio::sync::mpsc::unbounded_channel();
-    let (contents_tx, mut contents_rx) = tokio::sync::mpsc::unbounded_channel::<(Contents, Job)>();
-
-    spawn(inference_worker(
-        session,
-        batch_size,
-        chimp_image_rx,
-        prediction_tx,
-    ));
-
-    let mut tasks = JoinSet::new();
-
-    let mut well_locations = HashMap::new();
-    let mut well_contents = HashMap::new();
-
-    loop {
-        let timeout = if let Some(timeout) = args.timeout {
-            Either::Left(Delay::new(Duration::from_millis(timeout)))
-        } else {
-            Either::Right(std::future::pending())
-        };
-
-        select! {
-            biased;
-
-            Some((well_location, job)) = well_location_rx.recv() => {
-                if let Some(contents) = well_contents.remove(&job.id) {
-                    tasks.spawn(produce_response(contents, well_location, job, response_channel.clone()));
-                } else {
-                    well_locations.insert(job.id, well_location);
-                }
-            }
-
-            Some((contents, job)) = contents_rx.recv() => {
-                if let Some(well_location) = well_locations.remove(&job.id) {
-                    tasks.spawn(produce_response(contents, well_location, job, response_channel.clone()));
-                } else {
-                    well_contents.insert(job.id, contents);
-                }
-            }
-
-            Some(delivery) = job_consumer.next() =>  {
-                tasks.spawn(consume_job(delivery, input_width, input_height, chimp_image_tx.clone(), well_image_tx.clone()));
-            }
-
-            Some((well_image, job)) = well_image_rx.recv() =>  {
-                tasks.spawn(find_well_center(well_image, job, well_location_tx.clone()));
-            }
-
-            Some((bboxes, labels, _, masks, job)) = prediction_rx.recv() => {
-                tasks.spawn(postprocess_inference(bboxes, labels, masks, job, contents_tx.clone()));
-            }
-
-            _ = timeout => {
-                println!("Stopping: No jobs processed for {}ms", args.timeout.unwrap());
-                break;
-            }
-
-            else => break
-        }
+    let mut runtime_builder = tokio::runtime::Builder::new_multi_thread();
+    runtime_builder.enable_all();
+    if let Some(threads) = args.threads {
+        runtime_builder.worker_threads(threads);
     }
+    runtime_builder.build().unwrap().block_on(async {
+        let session = setup_inference_session(args.model).unwrap();
+        let input_width = session.inputs[0].dimensions[3].unwrap();
+        let input_height = session.inputs[0].dimensions[2].unwrap();
+        let batch_size = session.inputs[0].dimensions[0].unwrap().try_into().unwrap();
+    
+        let rabbitmq_client = setup_rabbitmq_client(args.rabbitmq_url).await.unwrap();
+        let job_channel = rabbitmq_client.create_channel().await.unwrap();
+        let response_channel = rabbitmq_client.create_channel().await.unwrap();
+        let mut job_consumer = setup_job_consumer(job_channel, args.rabbitmq_channel)
+            .await
+            .unwrap();
+    
+        let (chimp_image_tx, chimp_image_rx) = tokio::sync::mpsc::channel(batch_size);
+        let (well_image_tx, mut well_image_rx) = tokio::sync::mpsc::channel(batch_size);
+        let (well_location_tx, mut well_location_rx) =
+            tokio::sync::mpsc::unbounded_channel::<(Circle, Job)>();
+        let (prediction_tx, mut prediction_rx) = tokio::sync::mpsc::unbounded_channel();
+        let (contents_tx, mut contents_rx) = tokio::sync::mpsc::unbounded_channel::<(Contents, Job)>();
+    
+        spawn(inference_worker(
+            session,
+            batch_size,
+            chimp_image_rx,
+            prediction_tx,
+        ));
+    
+        let mut tasks = JoinSet::new();
+    
+        let mut well_locations = HashMap::new();
+        let mut well_contents = HashMap::new();
+    
+        loop {
+            let timeout = if let Some(timeout) = args.timeout {
+                Either::Left(Delay::new(Duration::from_millis(timeout)))
+            } else {
+                Either::Right(std::future::pending())
+            };
+    
+            select! {
+                biased;
+    
+                Some((well_location, job)) = well_location_rx.recv() => {
+                    if let Some(contents) = well_contents.remove(&job.id) {
+                        tasks.spawn(produce_response(contents, well_location, job, response_channel.clone()));
+                    } else {
+                        well_locations.insert(job.id, well_location);
+                    }
+                }
+    
+                Some((contents, job)) = contents_rx.recv() => {
+                    if let Some(well_location) = well_locations.remove(&job.id) {
+                        tasks.spawn(produce_response(contents, well_location, job, response_channel.clone()));
+                    } else {
+                        well_contents.insert(job.id, contents);
+                    }
+                }
+    
+                Some(delivery) = job_consumer.next() =>  {
+                    tasks.spawn(consume_job(delivery, input_width, input_height, chimp_image_tx.clone(), well_image_tx.clone()));
+                }
+    
+                Some((well_image, job)) = well_image_rx.recv() =>  {
+                    tasks.spawn(find_well_center(well_image, job, well_location_tx.clone()));
+                }
+    
+                Some((bboxes, labels, _, masks, job)) = prediction_rx.recv() => {
+                    tasks.spawn(postprocess_inference(bboxes, labels, masks, job, contents_tx.clone()));
+                }
+    
+                _ = timeout => {
+                    println!("Stopping: No jobs processed for {}ms", args.timeout.unwrap());
+                    break;
+                }
+    
+                else => break
+            }
+        }
+    });
 }
