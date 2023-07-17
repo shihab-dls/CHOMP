88eb22c46cb54f35941975a11d5a6c01afdb73b0
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-07-17 14:02:41 +0000

    Silence chimp_chomp build

--- chimp_chomp/build.rs
+++ chimp_chomp/build.rs
@@ -3,7 +3,6 @@ use std::{env, fs::copy, path::PathBuf};
 const MODEL_FILE: &str = "chimp.onnx";
 
 fn main() {
-    println!("Copying chimp.onnx");
     let model_src = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join(MODEL_FILE);
     let model_dst = PathBuf::from(env::var("OUT_DIR").unwrap())
         .parent()
